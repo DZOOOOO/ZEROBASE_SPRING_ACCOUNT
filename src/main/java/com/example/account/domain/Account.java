66f0ba72ad32f5ee9092cee08eb18717cefe1237
DZOOOOO <dzpro0327@naver.com>
2023-12-29 21:32:06 +0900

    embedded-redis M1(X)

---
+++ src/main/java/com/example/account/domain/Account.java
@@ -0,0 +1,23 @@
+package com.example.account.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String accountNumber;
+
+    @Enumerated(EnumType.STRING)
+    private AccountStatus accountStatus;
+
+}
---
+++ src/main/java/com/example/account/domain/AccountStatus.java
@@ -0,0 +1,6 @@
+package com.example.account.domain;
+
+public enum AccountStatus {
+    IN_USE, // 사용
+    UNREGISTERED // 해지
+}
