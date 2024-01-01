69bb0ea262970ae505d4399fb8dd4a7b0304df3d
DZOOOOO <dzpro0327@naver.com>
2024-01-01 14:23:28 +0900

    check

--- src/main/java/com/example/account/config/RedisRepositoryConfig.java
+++ src/main/java/com/example/account/config/RedisRepositoryConfig.java
@@ -5,8 +5,9 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class RedisRepositoryConfig {
     @Value("${spring.redis.host}")
     private String redisHost;
